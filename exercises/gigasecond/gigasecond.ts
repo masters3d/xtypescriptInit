e8761801e4ea17a7203ae36d2466041dea5c9738
J Cheyo Jimenez <cheyo@masters3d.com>
2017-02-16 15:53:08 -0800

    update gigassecond

--- exercises/gigasecond/gigasecond.ts
+++ exercises/gigasecond/gigasecond.ts
@@ -1,10 +1,11 @@
 
-class Gigasecond {
-    date: Date
-    constructor(date: Date){
-        this.date = date
+const gigamilisecond = 1000000000 * 1000
+ export default class Gigasecond {
+    currentDate: Date
+    constructor(date: Date) {
+        this.currentDate = date
     }
-
+    date(): Date {
+        return new Date(this.currentDate.valueOf() + gigamilisecond)
+    }
 }
-
-export default Gigasecond;
